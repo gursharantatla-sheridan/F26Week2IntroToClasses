4a3b88255947e4ed0817ed28548776ae850d167b
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-16 16:37:27 -0400

    constructors

---
+++ F26Week2IntroToClasses/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace F26Week2IntroToClasses
+{
+    public class Product
+    {
+        // fields
+        private int id;
+        private string _name;
+        private double _price;
+
+
+        // constructors
+        public Product()
+        {
+            id = 0;
+            _name = "";
+            _price = 0;
+        }
+
+        public Product(int id, string name, double price)
+        {
+            this.id = id;
+            _name = name;
+            _price = price;
+        }
+
+        // methods
+        public void Print()
+        {
+
+        }
+    }
+}
--- F26Week2IntroToClasses/Program.cs
+++ F26Week2IntroToClasses/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Product p1 = new Product();
+            Product p2 = new Product(101, "Laptop", 1200);
+
+            Product p3 = new();
+            Product p4 = new(102, "Mobile", 2000);
+
+            var p5 = new Product();
+            //var p6 = new();
         }
     }
 }
