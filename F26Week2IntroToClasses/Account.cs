cf126d3ccc89c2b449c51f0024a6113ace3ecf84
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-21 13:07:18 -0400

    account class

---
+++ F26Week2IntroToClasses/Account.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace F26Week2IntroToClasses
+{
+    public class Account
+    {
+        // prop + Tab
+        public int MyProperty { get; set; }
+
+        // propg + Tab
+        public int MyProperty1 { get; private set; }
+
+
+        // propfull + Tab
+        private double _balance;
+
+        public double Balance
+        {
+            get { return _balance; }
+            private set { _balance = value; }
+        }
+
+
+        public Account(double startingBalance)
+        {
+            Balance = startingBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount;
+        }
+    }
+}
